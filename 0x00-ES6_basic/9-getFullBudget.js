8a85fef761f745ff51b4a21ce1a83160014cf989
Metrinenaliaka <metrinenam@gmail.com>
2024-04-22 21:39:35 +0300

    fixed eslint errors

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from "./7-getBudgetObject";
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
