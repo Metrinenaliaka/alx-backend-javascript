1e20e3dc2bcb891e79214baa53f22a3c10b02ece
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2024-06-18 10:06:07 +0300

    fixed code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,24 @@
-function countStudents(file_path) {
-    const fs = require('fs');
-    try {
-        const data = fs.readFileSync(file_path, 'utf8').split('\n');
-        const headers = data.shift().split(',');
-        const students = data.map((line) => line.split(','));
-        const studentsByField = {};
-        students.forEach((student) => {
-            if (!studentsByField[student[3]]) {
-                studentsByField[student[3]] = [];
-            }
-            studentsByField[student[3]].push(student[0]);
-        });
-        console.log(`Number of students: ${students.length}`);
-        for (const field in studentsByField) {
-            if (field) {
-                const list = studentsByField[field];
-                console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-            }
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) throw Error('Cannot load the database');
+  const fileContent = fs.readFileSync(path, 'utf8');
+
+  const lines = fileContent.split('\n');
+  if (lines[lines.length - 1] === '') {
+    lines.pop();
+  }
+
+  const students = lines.map((line) => line.split(','));
+  const fields = [...new Set(students.map((student) => student[3]))];
+  fields.shift();
+
+  console.log(`Number of students: ${students.length - 1}`);
+  for (const field of fields) {
+    const fieldStudents = students.filter((student) => student[3] === field);
+    const fieldStudentsNames = fieldStudents.map((student) => student[0]);
+    console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudentsNames.join(', ')}`);
+  }
 }
+
 module.exports = countStudents;
